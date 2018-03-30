3cb4d24d360a4645c70baaeb8cfc8ba8791a3513
Welliton Gervickas <wellitogervickas@gmail.com>
2018-03-29 23:51:10 -0300

    Initial languages strings

--- src/models/languages/pt-br.js
+++ src/models/languages/pt-br.js
@@ -5,6 +5,21 @@ const ptBr = (() => {
   return {
     TITLES: {
       greetings: 'Olá, %s seja bem-vindo',
+      buy: 'Comprar',
+      cancel: 'Cancelar',
+      add: 'Adicionar',
+      finish: 'Finalizar',
+    },
+    WARNINGS: {
+      'empty-cart': 'Seu carrinho está vazio!',
+      'empty-stock': 'Esse produto não encontra-se em estoque!',
+    },
+    ERRORS: {
+      purchase: 'Houve um erro ao finalizar sua compra!',
+    },
+    SUCCESS: {
+      purchase: 'Compra efetuada com sucesso!',
+      'product-add': 'Produto adicionado com sucesso!'.
     },
   };
 
