3b357845ea7ee5450dc0d997b0682deaccb1cdbd
Welliton Gervickas <wellitogervickas@gmail.com>
2018-03-30 16:47:54 -0300

    Header and footer component registration

--- src/main.js
+++ src/main.js
@@ -7,15 +7,22 @@
   * Documentation of third party
   * Docs for Vue: https://vuejs.org/
   * Docs for VueRouter: https://router.vuejs.org/en/
+  * Docs for VueAxios: https://github.com/imcvampire/vue-axios
   *
 */
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-//Others Imports
+// External js
 import routes from './models/helpers/router-helper'
 import stores from './stores/store'
+
+// External css
+import 'normalize.css/normalize.css'
+import './assets/styles/main.scss'
 
 /**
   * Vue Project Uses
@@ -23,6 +30,7 @@ import stores from './stores/store'
 */
 
 Vue.use(VueRouter)
+Vue.use(VueAxios, axios)
 
 /**
   * Router Configuration
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,11 +3,22 @@
 </style>
 
 <template>
-  <div>Marvel Store </div>
+  <div>
+    <store-header></store-header>
+    <div class="container">Marvel Store </div>
+    <store-footer></store-footer>
+  </div>
 </template>
 
 <script>
+  import StoreHeader from '../content/header/store-header.vue'
+  import StoreFooter from '../content/footer/store-footer.vue'
+
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      StoreHeader,
+      StoreFooter
+    }
   }
 </script>
