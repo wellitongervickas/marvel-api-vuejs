e482cd6450d903fad8e9a6f7e73d014b18a8ff67
Welliton Gervickas <wellitogervickas@gmail.com>
2018-03-31 10:51:47 -0300

    First module of vuex

---
+++ src/views/content/header/menu/store-menu.vue
@@ -0,0 +1,17 @@
+<style lang="scss">
+  @import 'store-menu';
+</style>
+
+<template>
+  <nav>
+    <div class="container">
+      #Menu
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'StoreMenu'
+  }
+</script>
