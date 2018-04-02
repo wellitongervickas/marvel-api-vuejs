da71beb77a71c5acf098e0b45d526e81cc993214
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-01 23:42:33 -0300

    Update slideshow-helper.js

--- src/models/helpers/slideshow-helper.js
+++ src/models/helpers/slideshow-helper.js
@@ -26,11 +26,8 @@ const slideshowHelper = (() => {
   */
 
   function setInitialSliderStatus(sliderList) {
-
-    for (let i in sliderList) {
-      if (i == 0) sliderList[i].status = true;
-    }
-
+    
+    sliderList[0].status = true;
     return sliderList;
   };
 
