3ad673b3b01e7f5968ebee7fd50ec525f696ebe0
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-04 20:49:51 -0300

    Find image not available in thumbnail object and replace of local picture

--- src/models/class/product-class.js
+++ src/models/class/product-class.js
@@ -1,6 +1,8 @@
+import productHelper from '../helpers/product-helper';
+
 class Product {
   constructor(product) {
-    this.image = `${product.thumbnail.path}.${product.thumbnail.extension}`;
+    this.image = productHelper.validateImageNotFound(product.thumbnail.path, product.thumbnail.extension)
     this.title = product.title;
     this.creator = (product.creators.available > 0) ? product.creators.items[0].name : null;
   }
