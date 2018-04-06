02a23d16ad22bb496c91cb4fee4115942a47ab4b
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-06 00:10:20 -0300

    Insert creators in class of product

--- src/models/class/product-class.js
+++ src/models/class/product-class.js
@@ -8,7 +8,10 @@ class Product {
     this.id = product.id;
     this.image = productHelper.validateImageNotFound(product.thumbnail.path, product.thumbnail.extension);
     this.title = product.title;
-    this.creator = (product.creators.available > 0) ? product.creators.items[0].name : null;
+    this.creator = productHelper.getCreator(product.creators, 'editor');
+    this.penciler = productHelper.getCreator(product.creators, 'penciller');
+    this.writer = productHelper.getCreator(product.creators, 'writer');
+    this.coverArtist = productHelper.getCreator(product.creators, 'penciller (cover)');
   }
 };
 
