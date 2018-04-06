cf2040b54313d23b46af02418f5130128632e47a
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-05 21:33:22 -0300

    Remove coment and fix a getter method

--- src/stores/modules/products/getters.js
+++ src/stores/modules/products/getters.js
@@ -3,17 +3,5 @@
 import storageHelper from '../../../models/helpers/storage-helper';
 
 export default {
-  getProdutsFromLocal(state) {
-
-    // console.log('called');
-
-    // // Verify if products is saved in storage before try return from vuex
-    // const getFromStorage = storageHelper.get('products');
-
-    // if (getFromStorage.length > 1) {
-    //   return getFromStorage;
-    // }
-
-    return state.productsList;
-  }
+  getProdutsFromLocal: state => state.productsList
 }
