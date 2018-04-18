e7a044faec6c9510e861865edda5848f75bf5aae
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-17 22:01:41 -0300

    Initialized alert component

---
+++ src/components/alerts/float/store-alerts-float.vue
@@ -0,0 +1,21 @@
+<style lang="scss">
+  @import 'store-alerts-float';
+</style>
+
+<template>
+  <div class="alert-float">
+    <div class="alert-float-list">
+      <div class="alert-float-item">hello from alert</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import { mapGetters, mapState, mapActions  } from 'vuex';
+  import alertHelper from '../../../models/helpers/alert-helper';
+
+  export default {
+    name: 'StoreAlertsFloat'
+  }
+</script>
