2da97fd55c04940dd4e17a97c8196b43755e1b71
Welliton Gervickas <wellitogervickas@gmail.com>
2018-04-20 22:32:43 -0300

    Get from stores all status of availables filters

--- src/components/products-filter/store-products-filter.vue
+++ src/components/products-filter/store-products-filter.vue
@@ -6,41 +6,55 @@
   <div class="store-products-filter user-select">
     <div class="filter-nav-list container flex-around-center text-uppercase">
       <div class="filter-nav-item"><b>Browser By</b></div>
-
       <div
         class="filter-nav-item nav-item-dropdown"
-        @mouseleave="filterSeriesStatus = false"
-        @mouseover="filterSeriesStatus = true">
+        @mouseenter="disableAllNavs()"
+        @click="filterSeriesStatus = true">
         Series
       </div>
-
       <div
         class="filter-nav-item nav-item-dropdown"
-        @mouseleave="filterCharactersStatus = false"
-        @mouseover="filterCharactersStatus = true">
+        @mouseenter="disableAllNavs()"
+        @click="filterCharactersStatus = true">
         Characters
       </div>
-
       <div
         class="filter-nav-item nav-item-dropdown"
-        @mouseleave="filterCreatorsStatus = false"
-        @mouseover="filterCreatorsStatus = true">
+        @mouseenter="disableAllNavs()"
+        @click="filterCreatorsStatus = true">
         Creators
       </div>
-
     </div>
-
     <transition-group tag="div" class="filter-content-list container relative flex-around" name="list">
-      <div class="filter-content-item text-white" v-show="filterSeriesStatus" :key="0">Series</div>
-      <div class="filter-content-item text-white" v-show="filterCharactersStatus" :key="1">Characters</div>
-      <div class="filter-content-item text-white" v-show="filterCreatorsStatus" :key="2">Creators</div>
+      <div
+        class="filter-content-item text-white"
+        v-show="filterSeriesStatus"
+        @mouseleave="filterSeriesStatus = false"
+        :key="0">
+        {{getAvailableFilters.series}}
+      </div>
+      <div
+        class="filter-content-item text-white"
+        v-show="filterCharactersStatus"
+        @mouseleave="filterCharactersStatus = false"
+        :key="1">
+        {{getAvailableFilters.characters}}
+      </div>
+      <div
+        class="filter-content-item text-white"
+        v-show="filterCreatorsStatus"
+        @mouseleave="filterCreatorsStatus = false"
+        :key="2">
+        {{getAvailableFilters.creators}}
+      </div>
     </transition-group>
-
-
   </div>
 </template>
 
 <script>
+
+  import { mapGetters, mapActions , mapState } from 'vuex';
+
   export default {
     name: 'StoreProductsFilter',
     data() {
@@ -49,6 +63,16 @@
         filterCharactersStatus: false,
         filterCreatorsStatus: false,
       }
+    },
+    computed: {
+      ...mapGetters(['getAvailableFilters'])
+    },
+    methods: {
+      disableAllNavs() {
+        this.filterSeriesStatus = false;
+        this.filterCharactersStatus = false;
+        this.filterCreatorsStatus = false;
+      }
     }
-  }
+  };
 </script>
